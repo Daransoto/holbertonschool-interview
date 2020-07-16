ccd718112df53c3579363fb840e856910a729d2b
Darwin <901@holbertonschool.com>
2020-07-16 04:41:53 +0000

    first upload of lockboxes

---
+++ 0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module contains the function canUnlockAll.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Function that checks if all boxes can be unlocked.
+    """
+    boxes_amount = len(boxes)
+    locked = set([box for box in range(1, boxes_amount)])
+    current_keys = [*boxes[0]]
+    while current_keys:
+        test_key = current_keys.pop()
+        if test_key in locked:
+            locked.remove(test_key)
+            current_keys.extend(boxes[test_key])
+        if not len(locked):
+            return True
+    return False
