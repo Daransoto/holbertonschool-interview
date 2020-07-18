ea5a6d4376e3cb01217997d5aea9c71333368633
Darwin <901@holbertonschool.com>
2020-07-18 19:12:37 +0000

    Modified handling of edge cases

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,16 +8,11 @@ def canUnlockAll(boxes):
     """
     Function that checks if all boxes can be unlocked.
     """
-    if type(boxes) != list:
-        return False
     boxes_amount = len(boxes)
     if boxes_amount <= 1:
         return True
-    locked = set([box for box in range(1, boxes_amount)])
-    if type(boxes[0]) == list and boxes[0]:
-        current_keys = [*boxes[0]]
-    else:
-        current_keys = []
+    locked = set([box for box in range(1, boxes_amount)]
+    current_keys = [key for key in boxes[0]]
     while current_keys:
         test_key = current_keys.pop()
         if test_key in locked:
