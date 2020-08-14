82c469711f56d8a238ebe70ea7cbd2c3a8781001
Daransoto <901@holbertonschool.com>
2020-08-14 15:23:12 +0000

    Added case when any of the grids is null

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -10,6 +10,9 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j, unstable, temp[3][3];
 
+	if (!grid1 || !grid2)
+		return;
+
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
 			grid1[i][j] += grid2[i][j];
