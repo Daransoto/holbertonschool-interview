d62e5c6a6ea3b533f2ec9c8c6f0f26d3e3fda179
Daransoto <901@holbertonschool.com>
2020-09-01 02:51:44 +0000

    Changed input for enum of stdin and using variable for size

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,27 +2,25 @@
 """
 This script parses logs.
 """
+import sys
 
 
-res = {"size": 0, "200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+res = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
        "405": 0, "500": 0}
-i = 0
-while True:
-    try:
-        i += 1
-        logs = input()
+size = 0
+try:
+    for i, logs in enumerate(sys.stdin, 1):
         if logs.split(' ')[7] not in res.keys():
             continue
         res[logs.split(' ')[7]] += 1
-        res["size"] += int(logs.split(' ')[8])
+        size += int(logs.split(' ')[8])
         if i % 10 == 0:
-            print("{}: {:d}".format("File size", res["size"]))
+            print("{}: {:d}".format("File size", size))
             for key, value in sorted(res.items()):
-                if value > 0 and key != "size":
+                if value > 0:
                     print("{}: {:d}".format(key, value))
-    except:
-        print("{}: {:d}".format("File size", res["size"]))
-        for key, value in sorted(res.items()):
-            if value > 0 and key != "size":
-                print("{}: {:d}".format(key, value))
-        break
+finally:
+    print("{}: {:d}".format("File size", size))
+    for key, value in sorted(res.items()):
+        if value > 0:
+            print("{}: {:d}".format(key, value))
