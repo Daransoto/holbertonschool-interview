b06566cf4139f7c7bba9b5568ac69b80cdd8db6f
Daransoto <901@holbertonschool.com>
2020-09-01 01:46:27 +0000

    Not using 0 as a key for size

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,7 +4,7 @@ This script parses logs.
 """
 
 
-res = {"0": 0, "200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+res = {"size": 0, "200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
        "405": 0, "500": 0}
 i = 0
 while True:
@@ -14,17 +14,15 @@ while True:
         if logs.split(' ')[7] not in res.keys():
             continue
         res[logs.split(' ')[7]] += 1
-        res["0"] += int(logs.split(' ')[8])
+        res["size"] += int(logs.split(' ')[8])
         if i % 10 == 0:
+            print("{}: {:d}".format("File size", res["size"]))
             for key, value in sorted(res.items()):
-                if key == "0":
-                    print("{}: {:d}".format("File size", value))
-                elif value > 0:
+                if value > 0 and key != "size":
                     print("{}: {:d}".format(key, value))
     except:
+        print("{}: {:d}".format("File size", res["size"]))
         for key, value in sorted(res.items()):
-            if key == "0":
-                print("{}: {:d}".format("File size", value))
-            elif value > 0:
+            if value > 0 and key != "size":
                 print("{}: {:d}".format(key, value))
         break
