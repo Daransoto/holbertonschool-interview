38dc4e5b6e43cf5be0c51b86e22bb57258d5b51c
Daransoto <901@holbertonschool.com>
2020-09-01 02:57:07 +0000

    Changed way of checking data

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,17 +10,18 @@ res = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
 size = 0
 try:
     for i, logs in enumerate(sys.stdin, 1):
-        if logs.split(' ')[7] not in res.keys():
+        data = logs.split()
+        if len(data) < 2 or data[-2] not in res.keys():
             continue
-        res[logs.split(' ')[7]] += 1
-        size += int(logs.split(' ')[8])
+        res[data[-2]] += 1
+        size += int(data[-1])
         if i % 10 == 0:
-            print("{}: {:d}".format("File size", size))
-            for key, value in sorted(res.items()):
-                if value > 0:
-                    print("{}: {:d}".format(key, value))
+            print("{}: {}".format("File size", size))
+            for key in sorted(res.keys()):
+                if res[key] > 0:
+                    print("{}: {}".format(key, res[key]))
 finally:
-    print("{}: {:d}".format("File size", size))
-    for key, value in sorted(res.items()):
-        if value > 0:
-            print("{}: {:d}".format(key, value))
+    print("{}: {}".format("File size", size))
+    for key in sorted(res.keys()):
+        if res[key] > 0:
+            print("{}: {}".format(key, res[key]))
