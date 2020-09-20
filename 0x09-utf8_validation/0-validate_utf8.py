61af78cdf72b8f6cfc49c8a2ab9dd1cf4608e9ed
Daransoto <901@holbertonschool.com>
2020-09-20 12:31:26 -0500

    Checking just last byte

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -10,18 +10,20 @@ def validUTF8(data):
     ld = len(data)
     count = 0
     while i < ld:
-        if data[i] & 0x80 == 0:
+        byte = data[i] & 0xff
+        if byte & 0x80 == 0:
             i += 1
-        elif data[i] & 0xc0 == 0x80:
+        elif byte & 0xc0 == 0x80:
             return False
         else:
             test = 0x40
-            while data[i] & test:
+            while byte & test:
                 count += 1
                 test >>= 1
             i += 1
             for _ in range(count):
-                if i >= ld or data[i] & 0x80 != 0x80:
+                byte = data[i] & 0xff
+                if i >= ld or byte & 0x80 != 0x80:
                     return False
                 i += 1
     return count == 0
